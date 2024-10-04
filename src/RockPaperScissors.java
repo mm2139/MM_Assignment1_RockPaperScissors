19793d27c2c1ae12f4194546eb7468be4880358a
mmaalin <maalin.622139@swl.k12.oh.us>
2024-10-04 08:56:02 -0400

    not finished continue 10/7

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -1,5 +1,40 @@
-public class Main {
+import java.util.Scanner;
+
+public class RockPaperScissors {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+        char player1 = 'Z';
+        char player2 = 'Z';
+
+        System.out.println("Welcome to Rock, Paper, Scissors! Player 1, please input your choice: Rock, Paper, or Scissors? [R, P,S]");
+        DetermineInput(player1);
+        System.out.println("Player 2, please input your choice: Rock, Paper, or Scissors? [R, P,S]");
+        DetermineInput(player2);
+
     }
+
+    public static void DetermineInput(Character input) {
+
+        Scanner scan = new Scanner(System.in);
+        boolean validInput = false;
+
+        do {
+            if (scan.hasNextLine()) {
+                input = scan.next().charAt(0);
+                if (input == 'R' || input == 'P' || input == 'S') {
+                    validInput = true;
+                } else {
+                    System.out.println("You have inputted an invalid response. Please try again.");
+                    scan.nextLine();
+                }
+            } else {
+                System.out.println("You have inputted an invalid response. Please try again.");
+                scan.nextLine();
+            }
+        } while (!validInput);
+    }
+
 }
+
+
